72e22b2a0d71d92e52f36f3bcef8627ff1a463b0
GabsLeo <gabrieldemoraistaveira@gmail.com>
2021-11-01 23:07:09 -0300

    :package:  Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 function App() {
   return (
     <div className="App">
-      <h1>Olá Mundo!</h1>
+      <h1 className="text-primary" >Olá Mundo!</h1>
     </div>
   );
 }
 
+//Utilizamos className ao invés de "class"
 export default App;
