829dc1a2dcdad5860ec095d82426bc7e7a1c0a50
GabsLeo <gabrieldemoraistaveira@gmail.com>
2021-11-03 17:54:32 -0300

    :sparkles: Arquivo de Segurança

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,14 +10,14 @@ function App() {
       <Navbar />
 
       <div className="container">
-        <h1 className="text-primary py-3" >Dashboard de Vendas</h1>
+        <h1 className="text-primary py-3" >Dashboard de vendas</h1>
         <div className="row px-3">
           <div className="col-sm-6">
             <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
             <BarChart />
           </div>
           <div className="col-sm-6">
-            <h5>Todas as vendas</h5>
+            <h5 className="text-center text-secondary">Todas as vendas</h5>
             <DonutChart />
           </div>
         </div>
